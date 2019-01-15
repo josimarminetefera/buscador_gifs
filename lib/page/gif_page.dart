f2f71ef726c5ae743b6efebd3670c49d89ed6808
josimarminetefera <josimaminete@gmail.com>
2019-01-15 18:52:29 -0200

    Adicionado arquivos do projeto.

--- lib/page/gif_page.dart
+++ lib/page/gif_page.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:share/share.dart';
+import 'package:flutter_masked_text/flutter_masked_text.dart';
 
 class GifPage extends StatelessWidget {
 
